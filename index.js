79c2071e2f9305b7ce201486ffa62ccaf4187ce8
lotib <boit.sanssoif@gmail.com>
2015-11-07 12:54:04 +0100

    async download one after another

--- index.js
+++ index.js
@@ -11,11 +11,10 @@ var config = require('./podcasts_fetcher.json');
 function run(RSSFeed) {
 	var request = new Request(RSSFeed);
 	var feedparser = new FeedParser([]);
-	var cptEntries = 0;
 	var feedFolder = '';
+	var podcasts = [];
 
 	request.on('error', function(error) {
-		// handle any request errors
 		console.error(error);
 	});
 
@@ -25,7 +24,6 @@ function run(RSSFeed) {
 		if (res.statusCode !== 200) {
 			return this.emit('error', new Error('Bad status code'));
 		}
-
 		stream.pipe(feedparser);
 	});
 
@@ -48,53 +46,70 @@ function run(RSSFeed) {
 		var stream = this;
 		var meta = this.meta;
 		var item;
-
+		
 		while (item = stream.read()) {
 			parseItem(item);
-			cptEntries++;
 		}
 	});
 
 	feedparser.on('end', function() {
-		console.log(String(cptEntries).cyan + ' podcasts have been downloaded to '.yellow + feedFolder.cyan + ' folder'.yellow);
+		console.log(String(podcasts.length).cyan + ' podcasts have been downloaded to '.yellow + feedFolder.cyan + ' folder'.yellow);
+
+		
+		popAndSavePodcast();
 	});
+
+	function popAndSavePodcast()
+	{
+		var podcast = podcasts.pop();
+
+		if (podcast)
+		{
+			console.log(podcast.title, podcast.length);
+			
+			// saving playlist file
+			createFile(podcast.playlist, podcast.url_m, function(err) {
+				if (err) {
+					console.error(err);
+					console.error("The file [" + podcast.playlist + "] could not be saved :(");
+					return;
+				}
+				//console.log("The file [" + output + "] was saved :)");
+			});
+
+			if (config.media_download && podcast.length <= config.max_media_download_size) {
+				// downloading media file
+				// will pop the next podcast at the end of download
+				downloadFile(podcast.url, podcast.file, popAndSavePodcast);
+			}
+			else
+			{
+				popAndSavePodcast();
+			}
+		}
+		
+		
+	}
 
 	function parseItem(item) {
 		// \u00E0-\u00FC means to keep the accents :)
-		var title = item['title'].replace(/[^a-zA-Z- 0-9.\u00E0-\u00FC]/gi, '');;
-
+		var title = item['title'].replace(/[^a-zA-Z- 0-9.\u00E0-\u00FC]/gi, '');
 		var dateObj = new Date(item['pubDate']);
-		var media_date = sprintf('%d-%02d-%02d',
+		var date = sprintf('%d-%02d-%02d',
 					dateObj.getFullYear(),
 					dateObj.getMonth() + 1,
 					dateObj.getDate());
 
-		var media_length = parseInt(item['enclosures'][0]['length']);
-		var media_url_m = item['enclosures'][0]['url'];
-		var media_url = item['guid'];
-
-		// for remote listening
-		var playlist_file = feedFolder + media_date + '-' + title + '.m3u';
-		// for offline listening
-		/// \todo get extension from original file name
-		var media_file = feedFolder + media_date + '-' + title + '.mp3';
-
-		// saving playlist file
-		createFile(playlist_file, media_url_m, function(err) {
-			if (err) {
-				console.error(err);
-				console.error("The file [" + playlist_file + "] could not be saved :(");
-				return;
-			}
-			//console.log("The file [" + output + "] was saved :)");
-		});
-
-		// downloading media file
-		if (config.media_download && media_length <= config.max_media_download_size) {
-			downloadFile(media_url, media_file);
-		}
-
-		console.log(media_url, media_file, media_length);
+		var podcast = {
+			"title": title,
+			"date": date,
+			"length": parseInt(item['enclosures'][0]['length']),
+			"url_m": item['enclosures'][0]['url'],
+			"url": item['guid'],
+			"playlist": feedFolder + date + '-' + title + '.m3u',
+			"file": feedFolder + date + '-' + title + '.mp3'
+		};
+		podcasts.push(podcast);
 	}
 
 	function downloadFile(url, dest, callback) {
@@ -128,14 +143,14 @@ function run(RSSFeed) {
 			}
 		}
 	}
-
 }
 
 try {
 	// browsing feeds from configuration file
 	for (var i = 0; i < config.rss_feeds.length; i++) {
 		run(config.rss_feeds[i]);
-	}
+	}	
+	
 }
 catch (error) {
 	console.error('Error: ', error);
